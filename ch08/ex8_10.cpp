5a2f38a5da9719bd7a6153c725377464faf5d556
ChangXu <57488241+cx748@users.noreply.github.com>
2020-03-16 12:21:56 +0800

    Add files via upload

---
+++ ch08/ex8_10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::istringstream;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+int main()
+{
+    ifstream ifs("../data/book.txt");
+
+    if(!ifs)
+    {
+        cerr<<"No data"<<endl;
+        return -1;
+    }
+
+    vector<string> vec;
+    string line;
+    while(getline(ifs,line))
+        vec.push_back(line);
+
+    for (auto& s:vec){
+        istringstream iss(s);
+        string word;
+        while(iss >> word)
+            cout<<word<<endl;
+    }
+    return 0;
+}
